6eb64f2ba0c474224f325f8c28470c9837124fda
Guido Gauna <guidogauna9@gmail.com>
2023-11-18 02:34:34 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    })
-   );
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const corsOptions: CorsOptions = {
+    origin: true, // Permite cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  // Aplica el middleware CORS a nivel de aplicación
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
